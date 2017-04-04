369d1903c4043fd4066e7ec5c946876317ef1ad8
Arkhist <nideri.ben@gmail.com>
2017-04-05 01:12:48 +0200

    Made basic mod template and abstract mod class

---
+++ TemplateMod/TemplateMod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemplateMod
+{
+    public class TemplateMod : Pathfinder.PathfinderMod
+    {
+        public override string GetIdentifier()
+        {
+            return "Template Mod"
+        }
+
+        public override void Load()
+        {
+            Console.WriteLine("Loading Template Mod");
+        }
+    }
+}
