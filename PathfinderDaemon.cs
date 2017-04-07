c7e3ccd6a6fd3bfe5c74d214fb9e818c0cfaa923
Arkhist <nideri.ben@gmail.com>
2017-04-07 16:02:54 +0200

    Starting making Computer Loading stuff

---
+++ PathfinderDaemon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pathfinder
+{
+    public class PathfinderDaemon : Hacknet.Daemon
+    {
+        PathfinderDaemon(Hacknet.Computer computer, string serviceName, Hacknet.OS os) : base(computer, serviceName, os)
+        {
+            
+        }
+    }
+}
