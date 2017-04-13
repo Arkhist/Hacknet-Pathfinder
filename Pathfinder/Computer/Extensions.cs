495b3a1a6c3b23762f3e2eb95c4c3d4046d1a89d
George Albany <Megacake1234@gmail.com>
2017-04-12 23:44:37 -0400

    Added small extensions to OS and Computer.

---
+++ Pathfinder/Computer/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Pathfinder.Computer
+{
+    public static class Extensions
+    {
+        public static Daemon.Instance GetModdedDaemon(this Hacknet.Computer comp, Type modInterface)
+        {
+            foreach (var d in comp.daemons)
+                if ((d as Daemon.Instance)?.Interface?.GetType() == modInterface)
+                    return d as Daemon.Instance;
+            return null;
+        }
+
+        public static Daemon.Instance GetModdedDaemon<T>(this Hacknet.Computer comp)
+        {
+            return comp.GetModdedDaemon(typeof(T));
+        }
+    }
+}
---
+++ Pathfinder/OS/Extensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Hacknet;
+
+namespace Pathfinder.OS
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Only retrieves the OS's exes of the specifed type. Use <see cref="Extensions.GetExesFor(Hacknet.OS)"/> to get inherited types
+        /// </summary>
+        public static List<ExeModule> GetExesFor(this Hacknet.OS os, Type instanceType)
+        {
+            var result = new List<ExeModule>();
+            foreach (var e in os.exes)
+                if (e.GetType() == instanceType)
+                    result.Add(e);
+            return result;
+        }
+
+        /// <summary>
+        /// Retrieves all exes derived from T on the OS, Use <see cref="Extensions.GetExesFor(Hacknet.OS, Type)"/> to get only singular types
+        /// </summary>
+        public static List<T> GetExesFor<T>(this Hacknet.OS os) where T : ExeModule
+        {
+            var result = new List<T>();
+            foreach (var e in os.exes)
+                if (e is T)
+                    result.Add(e as T);
+            return result;
+        }
+
+        /// <summary>
+        /// Only retrieves the OS's modded exes of the specifed interface. Use <see cref="Extensions.GetModExeInterfaceFor(Hacknet.OS)"/> to get inherited types
+        /// </summary>
+        public static List<Executable.Instance> GetModExeInterfaceFor(this Hacknet.OS os, Type interfaceType)
+        {
+            var result = new List<Executable.Instance>();
+            foreach (var e in os.GetExesFor<Executable.Instance>())
+                if (e.Interface.GetType() == interfaceType)
+                    result.Add(e);
+            return result;
+        }
+
+        /// <summary>
+        /// Retrieves all modded exe interfaces derived from T on the OS, Use <see cref="Extensions.GetModExeInterfaceFor(Hacknet.OS, Type)"/> to get only singular types
+        /// </summary>
+        public static List<Executable.Instance> GetModExeInterfaceFor<T>(this Hacknet.OS os) where T : Executable.IInterface
+        {
+        	var result = new List<Executable.Instance>();
+        	foreach (var e in os.GetExesFor<Executable.Instance>())
+        		if (e.Interface is T)
+        			result.Add(e);
+        	return result;
+        }
+    }
+}
