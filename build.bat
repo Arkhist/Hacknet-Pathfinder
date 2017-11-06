34fc424cccbdcb39d1edfb0f8197403b51b2fd6c
George Albany <Megacake1234@gmail.com>
2017-11-05 19:43:29 -0500

    Fixed improper license name Automated release on XamarinStudio and through build.bat Pushed Pathfinder.dll to lib instead Added proper version to solution

--- build.bat
+++ build.bat
@@ -15,4 +15,6 @@ call PathfinderPatcher.exe -pathfinderDir "..\bin\Release" -exeDir "%steamDir%\s
 
 set msBuildDir=
 
+zipjs.bat zipItem -source ./PathfinderPatcher.exe -source ./Pathfinder.dll -source ./Mono.Cecil.dll -source ./Mono.Cecil.Inject.dll -source ./Cecil_LICENSE.txt -source ./Cecil_Inject_LICENSE.txt -source ../README.md -destination ../releases/Pathfinder.Release.V_.zip -keep yes -force yes
+
 cd ..
