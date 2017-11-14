2bfac0de0bc57084436cdce86f8de90be82897d2
George Albany <Megacake1234@gmail.com>
2017-11-13 20:59:37 -0500

    Fixing some mistakes

--- Pathfinder/Internal/ExecutionOverride.cs
+++ Pathfinder/Internal/ExecutionOverride.cs
@@ -39,11 +39,11 @@ namespace Pathfinder.Internal
                 if (canRun)
                     os.addExe(new PortHackExe(e.Destination, os));
                 else if (firewallActive)
-                    os.WriteF("{0} -\n{1}",
+                    os.Write("{0} -\n{1}",
                               LocaleTerms.Loc("Target Machine Rejecting Syndicated UDP Traffic"),
                               LocaleTerms.Loc("Bypass Firewall to allow unrestricted traffic"));
                 else
-                    os.WriteF("{0} - \n{1}\n",
+                    os.Write("{0} - \n{1}\n",
                               LocaleTerms.Loc("Too Few Open Ports to Run"),
                               LocaleTerms.Loc("Open Additional Ports on Target Machine"));
             }
@@ -58,7 +58,7 @@ namespace Pathfinder.Internal
                 var os = e.OS;
                 int i;
                 var c = os.GetCurrentComputer();
-                os.WriteF("Probing {0}...\n", c.ip);
+                os.Write("Probing {0}...\n", c.ip);
                 for (i = 0; i < 10; i++)
                 {
                     Thread.Sleep(80);
@@ -67,22 +67,22 @@ namespace Pathfinder.Internal
                 os.Write("\nProbe Complete - Open ports:\n").Write("---------------------------------");
                 if (Port.Instance.compToInst.ContainsKey(c)) foreach (var ins in Port.Instance.compToInst[c])
                     {
-                        os.WriteF("Port#: {0} - {1}{2}", ins.Port.PortDisplay, ins.Port.PortName, (ins.Unlocked ? "OPEN" : ""));
+                        os.Write("Port#: {0} - {1}{2}", ins.Port.PortDisplay, ins.Port.PortName, (ins.Unlocked ? "OPEN" : ""));
                         Thread.Sleep(120);
                     }
                 for (i = 0; i < c.ports.Count; i++)
                 {
-                    os.WriteF("Port#: {0} - {1}{2}", c.GetDisplayPortNumberFromCodePort(c.ports[i]),
+                    os.Write("Port#: {0} - {1}{2}", c.GetDisplayPortNumberFromCodePort(c.ports[i]),
                               PortExploits.services[c.ports[i]],
                               (c.portsOpen[i] > 0 ? " : OPEN" : ""));
                     Thread.Sleep(120);
                 }
                 os.Write("---------------------------------")
-                  .WriteF("Open Ports Required for Crack : {0}", Math.Max(c.portsNeededForCrack + 1, 0));
+                  .Write("Open Ports Required for Crack : {0}", Math.Max(c.portsNeededForCrack + 1, 0));
                 if (c.hasProxy)
-                    os.WriteF("Proxy Detected : {0}", (c.proxyActive ? "ACTIVE" : "INACTIVE"));
+                    os.Write("Proxy Detected : {0}", (c.proxyActive ? "ACTIVE" : "INACTIVE"));
                 if (c.firewall != null)
-                    os.WriteF("Firewall Detected : {0}", (c.firewall.solved ? "SOLVED" : "ACTIVE"));
+                    os.Write("Firewall Detected : {0}", (c.firewall.solved ? "SOLVED" : "ACTIVE"));
             }
             if (e[0].ToLower() == "help" || e[0].ToLower() == "man" || e[0] == "?")
             {
