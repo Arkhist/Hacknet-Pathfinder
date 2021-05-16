e3a4b39e36b27bbefb47c766e5eba3e8ce7ffe5f
Aaron Robinson <aaronrobin1234@gmail.com>
2021-05-16 00:30:23 -0500

    Start on a PathfinderAPI and base game fixes

---
+++ PathfinderAPI/BaseGameFixes/DoesNotHaveFlags.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using Hacknet;
+
+namespace Pathfinder.BaseGameFixes
+{
+    [HarmonyPatch]
+    internal static class DoesNotHaveFlags
+    {
+        [HarmonyILManipulator]
+        [HarmonyPatch(typeof(SerializableCondition), nameof(SerializableCondition.Deserialize))]
+        internal static void AddDoesNotHaveFlagsIL(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            var add = AccessTools.Method(typeof(Dictionary<string, Func<System.Xml.XmlReader, SerializableCondition>>), "Add", new Type[] { typeof(string), typeof(Func<System.Xml.XmlReader, SerializableCondition>) });
+
+            c.GotoNext(MoveType.After,
+                x => x.MatchCallvirt(add)
+            );
+
+            c.Emit(OpCodes.Ldloc_0);
+            c.Emit(OpCodes.Ldstr, "DoesNotHaveFlags");
+            c.Emit(OpCodes.Ldnull);
+            c.Emit(OpCodes.Ldftn, AccessTools.Method(typeof(SCDoesNotHaveFlags), nameof(SCDoesNotHaveFlags.DeserializeFromReader)));
+            c.Emit(OpCodes.Newobj, AccessTools.Constructor(typeof(Func<System.Xml.XmlReader, SerializableCondition>), new Type[] { typeof(object), typeof(IntPtr) }));
+            c.Emit(OpCodes.Callvirt, add);
+        }
+    }
+}
---
+++ PathfinderAPI/PathfinderAPIPlugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BepInEx.Hacknet;
+
+namespace Pathfinder
+{
+    [BepInPlugin(ModGUID, ModName, ModVer)]
+    public class PathfinderAPIPlugin : HacknetPlugin
+    {
+        public const string ModGUID = "com.Pathfinder.API";
+        public const string ModName = "PathfinderAPI";
+        public const string ModVer = "1.0.0";
+
+        public override bool Load()
+        {
+            HarmonyInstance.PatchAll(typeof(PathfinderAPIPlugin).Assembly);
+            return true;
+        }
+    }
+}
