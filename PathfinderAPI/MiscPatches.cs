46296b228a3c0da7d76fa3b8d2a94a6abbcc81fe
Aaron Robinson <aaronrobin1234@gmail.com>
2021-06-08 04:16:30 -0500

    Remove Program.Main try-catch when compiled as debug

--- PathfinderAPI/MiscPatches.cs
+++ PathfinderAPI/MiscPatches.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Text;
 using System.Diagnostics;
+using System.Linq;
+using System.Xml;
 using HarmonyLib;
 using MonoMod.Cil;
 using Mono.Cecil.Cil;
 using BepInEx.Hacknet;
+using BepInEx.Logging;
+using Hacknet;
+using Microsoft.Xna.Framework;
 
 namespace Pathfinder
 {
@@ -47,5 +52,26 @@ namespace Pathfinder
                 builder.Append($" IL<0x{frame.GetILOffset().ToString("X4")}>");
             });
         }
+        
+#if DEBUG
+        [HarmonyILManipulator]
+        [HarmonyPatch(typeof(Program), nameof(Program.Main))]
+        internal static void StopCatchingExceptionsIL(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            
+            foreach (var exHandler in il.Body.ExceptionHandlers)
+            {
+                c.Goto(exHandler.HandlerEnd, MoveType.After);
+                var end = c.Index;
+                c.Goto(exHandler.HandlerStart, MoveType.Before);
+                c.RemoveRange(end - c.Index);
+            }
+            
+            il.Body.ExceptionHandlers.Clear();
+            
+            foreach (var brokenLabel in il.Labels.Where(x => !c.Instrs.Contains(x.Target))) brokenLabel.Target = c.Instrs.Last();
+        }
+#endif
     }
 }
