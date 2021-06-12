3e0c78d447e1c463d58307fc4fde8d701ced428e
Aaron Robinson <aaronrobin1234@gmail.com>
2021-06-11 21:05:17 -0500

    Add events for load/unload of extensions and OS update

---
+++ PathfinderAPI/Event/Gameplay/OSUpdateEvent.cs
@@ -0,0 +1,27 @@
+﻿using Hacknet;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+
+namespace Pathfinder.Event.Gameplay
+{
+    [HarmonyPatch]
+    public class OSUpdateEvent : PathfinderEvent
+    {
+        public OS OS { get; }
+        public GameTime GameTime { get; }
+
+        public OSUpdateEvent(OS os, GameTime gameTime)
+        {
+            OS = os;
+            GameTime = gameTime;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(OS), nameof(OS.Update))]
+        internal static void OSUpdatePostfix(OS __instance, GameTime gameTime)
+        {
+            var osUpdate = new OSUpdateEvent(__instance, gameTime);
+            EventManager<OSUpdateEvent>.InvokeAll(osUpdate);
+        }
+    }
+}
---
+++ PathfinderAPI/Event/Loading/ExtensionLoadEvent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Hacknet;
+using Hacknet.Extensions;
+using Hacknet.Gui;
+using Hacknet.Screens;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+
+namespace Pathfinder.Event.Loading
+{
+    [HarmonyPatch]
+    public class ExtensionLoadEvent : PathfinderEvent
+    {
+        public ExtensionInfo Info { get; }
+        public bool Unload { get; }
+
+        public ExtensionLoadEvent(ExtensionInfo info, bool unload)
+        {
+            Info = info;
+            Unload = unload;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ExtensionsMenuScreen), nameof(ExtensionsMenuScreen.ActivateExtensionPage))]
+        internal static void ExtensionLoadPostfix(ExtensionInfo info)
+        {
+            var loadEvent = new ExtensionLoadEvent(info, false);
+            EventManager<ExtensionLoadEvent>.InvokeAll(loadEvent);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(OS), nameof(OS.quitGame))]
+        internal static void OSQuitPrefix()
+        {
+            if (Settings.IsInExtensionMode)
+            {
+                var unloadEvent = new ExtensionLoadEvent(ExtensionLoader.ActiveExtensionInfo, true);
+                EventManager<ExtensionLoadEvent>.InvokeAll(unloadEvent);
+            }
+        }
+
+        // I would hook Hacknet.Screens.DrawExtensionInfoDetail instead, but for some reason that method is cursed, so I look here instead
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Button), nameof(Button.doButton), new Type[] { typeof(int), typeof(int), typeof(int), typeof(int), typeof(int), typeof(string), typeof(Color?) })]
+        [HarmonyBefore("BepInEx.Hacknet.Chainloader")]
+        internal static void OnBackButtonPressPostfix(int myID, bool __result)
+        {
+            if (myID == 7900040 && __result)
+            {
+                var unloadEvent = new ExtensionLoadEvent(ExtensionLoader.ActiveExtensionInfo, true);
+                EventManager<ExtensionLoadEvent>.InvokeAll(unloadEvent);
+            }
+        }
+    }
+}
