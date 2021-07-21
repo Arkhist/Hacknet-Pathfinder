56c926ac2e5ef560bec31009be105b2317863bdf
Aaron Robinson <aaronrobin1234@gmail.com>
2021-07-20 21:46:39 -0500

    Use less refs in NodeLookup to prevent unboxing

--- PathfinderAPI/BaseGameFixes/Performance/NodeLookup.cs
+++ PathfinderAPI/BaseGameFixes/Performance/NodeLookup.cs
@@ -9,21 +9,21 @@ namespace Pathfinder.BaseGameFixes.Performance
     {
         [HarmonyPostfix]
         [HarmonyPatch(typeof(ComputerLoader), nameof(ComputerLoader.loadComputer))]
-        internal static void PopulateOnComputerCreation(ref object __result)
+        internal static void PopulateOnComputerCreation(object __result)
         {
             ComputerLookup.PopulateLookups((Computer) __result);
         }
 
         [HarmonyPostfix]
         [HarmonyPatch(typeof(Computer), nameof(Computer.load))]
-        internal static void PopulateOnComputerLoad(ref Computer __result)
+        internal static void PopulateOnComputerLoad(Computer __result)
         {
             ComputerLookup.PopulateLookups(__result);
         }
         
         [HarmonyPrefix]
         [HarmonyPatch(typeof(ComputerLoader), nameof(ComputerLoader.findComp))]
-        internal static bool ModifyComputerLoaderLookup(ref Computer __result, ref string target)
+        internal static bool ModifyComputerLoaderLookup(out Computer __result, string target)
         {
             __result = ComputerLookup.FindById(target);
             return false;
@@ -31,7 +31,7 @@ namespace Pathfinder.BaseGameFixes.Performance
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(Programs), nameof(Programs.getComputer))]
-        internal static bool ModifyProgramsLookup(ref Computer __result, ref string ip_Or_ID_or_Name)
+        internal static bool ModifyProgramsLookup(out Computer __result, string ip_Or_ID_or_Name)
         {
             __result = ComputerLookup.Find(ip_Or_ID_or_Name);
             return false;
