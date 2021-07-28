ecd80a87cb618ad2a29512aaf4c77aa9a25d6c39
Maowcraft <aidancrafter@gmail.com>
2021-07-28 02:41:04 -0400

    Reduce unnecessary instructions

--- PathfinderAPI/BaseGameFixes/ListingServerFixes.cs
+++ PathfinderAPI/BaseGameFixes/ListingServerFixes.cs
@@ -16,10 +16,6 @@ namespace Pathfinder.BaseGameFixes
         [HarmonyPatch(typeof(SAAddMissionToHubServer), nameof(SAAddMissionToHubServer.Trigger))]
         internal static void NullCheckOnAssignmentTag(ILContext il)
         {
-            // Adds a local (index 8) of type 'string' to the method
-            il.Body.Variables.Add(new VariableDefinition(
-                il.Module.ImportReference(typeof(string))));
-            
             var c = new ILCursor(il);
             
             c.GotoNext(x => x.MatchCallvirt(AccessTools.Method(typeof(string), nameof(string.ToLower))));
@@ -27,18 +23,13 @@ namespace Pathfinder.BaseGameFixes
             var start = c.DefineLabel(); // IL_0123
             var end   = c.DefineLabel(); // IL_0132
 
-            var tagLoc = 8; // The local in which AssignmentTag is stored
-            
-            c.Emit(OpCodes.Stloc, tagLoc);
-            c.Emit(OpCodes.Ldloc, tagLoc);
-            c.Emit(OpCodes.Ldnull);
-            c.Emit(OpCodes.Ceq);
+            c.Emit(OpCodes.Dup);
             c.Emit(OpCodes.Brfalse, start);
+            c.Emit(OpCodes.Pop);
             c.Emit(OpCodes.Ldc_I4_0);
             c.Emit(OpCodes.Br, end);
-
-            // Target an emitted instruction that comes before the String::ToLower call
-            start.Target = c.Emit(OpCodes.Ldloc, tagLoc).Prev;
+            
+            start.Target = c.Next;
 
             c.GotoNext(x => x.MatchCallvirt(AccessTools.Method(typeof(MissionListingServer), nameof(MissionListingServer.addMisison))));
 
