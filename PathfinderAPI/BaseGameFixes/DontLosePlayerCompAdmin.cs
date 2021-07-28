59235b30254eb044f09d1d1b9c32911740d4bc53
Aaron Robinson <aaronrobin1234@gmail.com>
2021-07-28 02:11:04 -0500

    Fix losing of admin on playerComp

---
+++ PathfinderAPI/BaseGameFixes/DontLosePlayerCompAdmin.cs
@@ -0,0 +1,36 @@
+﻿using Hacknet;
+using HarmonyLib;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+
+namespace Pathfinder.BaseGameFixes
+{
+    [HarmonyPatch]
+    internal static class DontLosePlayerCompAdmin
+    {
+        [HarmonyILManipulator]
+        [HarmonyPatch(typeof(SAChangeIP), nameof(SAChangeIP.Trigger))]
+        internal static void NotifyOSOfPlayerCompIPChange(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            c.GotoNext(MoveType.After,
+                x => x.MatchLdloc(1),
+                x => x.MatchLdarg(0),
+                x => x.MatchLdfld(AccessTools.Field(typeof(SAChangeIP), nameof(SAChangeIP.NewIP))),
+                x => x.MatchStfld(AccessTools.Field(typeof(Computer), nameof(Computer.ip)))
+            );
+
+            c.Emit(OpCodes.Ldarg_1);
+            c.Emit(OpCodes.Castclass, typeof(OS));
+            c.Emit(OpCodes.Ldloc_1);
+            c.EmitDelegate<System.Action<OS, Computer>>((os, comp) =>
+            {
+                if (comp.idName == "playerComp")
+                {
+                    os.thisComputerIPReset();
+                }
+            });
+        }
+    }
+}
