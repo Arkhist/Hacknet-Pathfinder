f5a99399161b58f031602ae96bc98f6c78590ea4
Aaron Robinson <aaronrobin1234@gmail.com>
2021-07-29 05:34:49 -0500

    Load DLCCredits daemon properly

--- PathfinderAPI/Replacements/ContentLoader.cs
+++ PathfinderAPI/Replacements/ContentLoader.cs
@@ -733,6 +733,7 @@ namespace Pathfinder.Replacements
                 }
                 
                 credits.ConditionalActionsToLoadOnButtonPress = info.Attributes.GetString("ConditionalActionSetToRunOnButtonPressPath", null);
+                comp.daemons.Add(credits);
             });
             executor.RegisterExecutor("Computer.CreditsDaemon", (exec, info) =>
             {
