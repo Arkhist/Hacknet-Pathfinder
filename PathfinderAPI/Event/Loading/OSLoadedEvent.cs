c5277bc103483a2d167569d4ace32ec78f5321b2
Aaron Robinson <aaronrobin1234@gmail.com>
2021-08-27 17:37:48 -0500

    Add OSLoaded event

---
+++ PathfinderAPI/Event/Loading/OSLoadedEvent.cs
@@ -0,0 +1,19 @@
+﻿using Hacknet;
+using HarmonyLib;
+
+namespace Pathfinder.Event.Loading
+{
+    public class OSLoadedEvent : PathfinderEvent
+    {
+        public OS Os { get; }
+
+        public OSLoadedEvent(OS os)
+        {
+            Os = os;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(OS), nameof(OS.LoadContent))]
+        private static void OSLoadPostfix(OS __instance) => EventManager<OSLoadedEvent>.InvokeAll(new OSLoadedEvent(__instance));
+    }
+}
