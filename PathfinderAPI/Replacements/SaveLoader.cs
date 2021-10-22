1ae4a97ee461894c58ddacf70822e2aaafebe739
George L. Albany <Megacake1234@gmail.com>
2021-10-22 18:46:24 -0400

    Added RegisterExecutor(Type, string, ParseOption) for 	ContentLoader 	ExtensionInfoLoader 	MissionLoader 	SaveLoader Redirected RegisterExecutor<T>(string, ParseOption) to call the runtime type version Added consistent safety checks for RegisterExecutor(Type, string, ParseOption)

--- PathfinderAPI/Replacements/SaveLoader.cs
+++ PathfinderAPI/Replacements/SaveLoader.cs
@@ -44,12 +44,19 @@ namespace Pathfinder.Replacements
         
         private static readonly List<SaveExecutorHolder> CustomExecutors = new List<SaveExecutorHolder>();
 
-        public static void RegisterExecutor<T>(string element, ParseOption options = ParseOption.None) where T : SaveExecutor, new()
+        public static void RegisterExecutor<T>(string element, ParseOption options = ParseOption.None) where T : SaveExecutor, new() => RegisterExecutor(typeof(T), element, options);
+        public static void RegisterExecutor(Type executorType, string element, ParseOption options = ParseOption.None)
         {
+            if (!typeof(SaveExecutor).IsAssignableFrom(executorType))
+                throw new ArgumentException("Type of executor registered must inherit from Pathfinder.Replacements.SaveLoader.SaveExecutor!", nameof(executorType));
+
+            if(!executorType.GetConstructors().Any(ctor => ctor.GetParameters().Length == 0))
+                throw new ArgumentException("Type of executor registered must have a default constructor", nameof(executorType));
+
             CustomExecutors.Add(new SaveExecutorHolder
             {
                 Element = element,
-                ExecutorType = typeof(T),
+                ExecutorType = executorType,
                 Options = options
             });
         }
