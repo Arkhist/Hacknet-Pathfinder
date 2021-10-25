dff1637d467e8d0499b21c5e04805c318b1db8cc
George L. Albany <Megacake1234@gmail.com>
2021-10-25 08:36:44 +0000

    Fixed inverse Contains check for ExeModule.Kill

--- PathfinderAPI/Executable/ExeModuleExtensions.cs
+++ PathfinderAPI/Executable/ExeModuleExtensions.cs
@@ -7,7 +7,7 @@ namespace Pathfinder.Executable
     {
         public static bool Kill(this ExeModule module)
         {
-            if(module.os.exes.Contains(module))
+            if(!module.os.exes.Contains(module))
                 return false;
             module.Killed();
             return module.os.exes.Remove(module);
