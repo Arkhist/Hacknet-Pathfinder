1697534836d8420b97fc5a019d12b0210c2cc2db
George L. Albany <Megacake1234@gmail.com>
2021-10-25 03:40:24 -0400

    Added GameExecutable according to #125 GameExecutable is added to OS.exes via ExecutableManager.AddGameExecutable Added HarmonyILManipulators for calling GameExecutable.Completed Marked BaseExecutable.GetIdentifier as Obsolete Made BaseExecutable.Args not readonly anymore

--- PathfinderAPI/Executable/ExecutableManager.cs
+++ PathfinderAPI/Executable/ExecutableManager.cs
@@ -8,9 +8,13 @@ using Pathfinder.Event;
 using Pathfinder.Event.Loading;
 using Pathfinder.Event.Gameplay;
 using Microsoft.Xna.Framework;
+using MonoMod.Cil;
+using HarmonyLib;
+using Mono.Cecil.Cil;
 
 namespace Pathfinder.Executable
 {
+    [HarmonyPatch]
     public static class ExecutableManager
     {
         private struct CustomExeInfo
@@ -42,7 +46,14 @@ namespace Pathfinder.Executable
             if (!exe.HasValue)
                 return;
             var location = new Rectangle(e.OS.ram.bounds.X, e.OS.ram.bounds.Y + RamModule.contentStartOffset, RamModule.MODULE_WIDTH, (int)OS.EXE_MODULE_HEIGHT);
-            e.OS.addExe((BaseExecutable)Activator.CreateInstance(exe.Value.ExeType, new object[] { location, e.OS, e.Arguments.ToArray() }));
+            if(exe.Value.ExeType.IsSubclassOf(typeof(GameExecutable)))
+                e.OS.AddGameExecutable(
+                    (GameExecutable)Activator.CreateInstance(exe.Value.ExeType, null),
+                    location,
+                    e.Arguments.ToArray()
+                );
+            else
+                e.OS.addExe((BaseExecutable)Activator.CreateInstance(exe.Value.ExeType, new object[] { location, e.OS, e.Arguments.ToArray() }));
             e.Result = ExecutionResult.StartupSuccess;
         }
 
@@ -78,6 +89,88 @@ namespace Pathfinder.Executable
         public static void UnregisterExecutable(Type exeType)
         {
             CustomExes.RemoveAll(x => x.Value.ExeType == exeType);
+        }
+
+        private static void AddGameExecutable(this OS os, GameExecutable exe, Rectangle location, string[] args)
+        {
+            exe.Assign(location, os, args);
+            var computer = os.connectedComp ?? os.thisComputer;
+            try
+            {
+                if(exe.needsProxyAccess && computer.proxyActive)
+                {
+                    exe.OnProxyBypassFailure();
+                    if(!exe.IgnoreProxyFailPrint)
+                        os.write(LocaleTerms.Loc("Proxy Active -- Cannot Execute"));
+                    return;
+                }
+
+                if(os.ramAvaliable >= exe.ramCost)
+                {
+                    exe.OnInitialize();
+                    if(exe.CanAddToSystem)
+                        os.exes.Add(exe);
+                    return;
+                }
+
+                exe.OnNoAvailableRam();
+                if(!exe.IgnoreMemoryBehaviorPrint)
+                {
+                    os.ram.FlashMemoryWarning();
+                    os.write(LocaleTerms.Loc("Insufficient Memory"));
+                }
+            }
+            catch(Exception e)
+            {
+                if(exe.CatchException(e))
+                    throw e;
+            }
+        }
+
+        [HarmonyILManipulator]
+        [HarmonyPatch(typeof(OS), nameof(OS.Update), typeof(GameTime), typeof(bool), typeof(bool))]
+        private static void onOSUpdate(ILContext il)
+        {
+            var c = new ILCursor(il);
+
+            c.GotoNext(MoveType.Before,
+                x => x.MatchNop(),
+                x => x.MatchLdarg(0),
+                x => x.MatchLdfld(AccessTools.Field(typeof(OS), nameof(OS.exes))),
+                x => x.MatchLdloc(1),
+                x => x.MatchCallvirt(AccessTools.Method(typeof(List<ExeModule>), nameof(List<ExeModule>.RemoveAt), new[] {typeof(int)}))
+            );
+
+            c.Emit(OpCodes.Ldarg, 0);
+            c.Emit(OpCodes.Ldloc, 1);
+
+            c.EmitDelegate<Action<OS, int>>((os, index) =>
+            {
+                if(os.exes[index] is GameExecutable exe)
+                    exe.Completed();
+            });
+        }
+
+        [HarmonyILManipulator]
+        [HarmonyPatch(typeof(Programs), nameof(Programs.kill), typeof(string[]), typeof(OS))]
+        private static void onProgramsKill(ILContext il)
+        {
+            var c = new ILCursor(il);
+
+            c.GotoNext(MoveType.After,
+                x => x.MatchLdloc(1),
+                x => x.MatchCallvirt(AccessTools.Method(typeof(List<ExeModule>), "get_Item", new[] {typeof(int)})),
+                x => x.MatchCallvirt(AccessTools.Method(typeof(ExeModule), nameof(ExeModule.Killed)))
+            );
+
+            c.Emit(OpCodes.Ldarg, 0);
+            c.Emit(OpCodes.Ldloc, 1);
+
+            c.EmitDelegate<Action<OS, int>>((os, index) =>
+            {
+                if(os.exes[index] is GameExecutable exe)
+                    exe.Completed();
+            });
         }
     }
 }
---
+++ PathfinderAPI/Executable/GameExecutable.cs
@@ -0,0 +1,183 @@
+using System.Runtime.InteropServices;
+using System;
+using System.Globalization;
+using Hacknet;
+using Microsoft.Xna.Framework;
+
+namespace Pathfinder.Executable
+{
+    public enum CompletionResult
+    {
+        Error = -1,
+        Running,
+        Failure,
+        Killed,
+        Success,
+    }
+    public class GameExecutable : BaseExecutable
+    {
+        /// <summary>
+        /// Lifetime value of the executable, added to upon being added to the OS on every Update
+        /// </summary>
+        /// <value></value>
+        public float Lifetime { get; set; }
+        private CompletionResult _Result = CompletionResult.Running;
+        /// <summary>
+        /// Status of the executable, sets isExiting if not set to CompletionResult.Running
+        /// </summary>
+        /// <value></value>
+        public CompletionResult Result
+        {
+            get => _Result;
+            set
+            {
+                _Result = value;
+                isExiting = Result != CompletionResult.Running;
+            }
+        }
+        /// <summary>
+        /// Determines whether the executable is added to the OS when executed
+        /// </summary>
+        public virtual bool CanAddToSystem { get; set; }
+        /// <summary>
+        /// Written to the terminal if failed or errored when not null
+        /// </summary>
+        /// <value></value>
+        public virtual string ErrorReturn { get; set; }
+        /// <summary>
+        /// Determines whether the vanilla proxy fail behavior should also happen
+        /// </summary>
+        /// <value></value>
+        public virtual bool IgnoreProxyFailPrint { get; set; }
+        /// <summary>
+        /// Determines whether the vanilla no available memory fail behavior should also happen
+        /// </summary>
+        /// <value></value>
+        public virtual bool IgnoreMemoryBehaviorPrint { get; set; }
+
+        public GameExecutable() : base(default, null, null)
+        {
+        }
+
+        public void Assign(Rectangle location, OS os, string[] args)
+        {
+            this.os = os;
+            bounds = location;
+            Args = args;
+        }
+
+        /// <summary>
+        /// Called immediately before being added to OS.exes.
+        /// </summary>
+        public virtual void OnInitialize() {}
+
+        /// <summary>
+        /// Called immediately before being removed from OS.exes.
+        /// Called when Result is CompletionResult.Error.
+        /// </summary>
+        public virtual void OnCompleteError() {}
+        /// <summary>
+        /// Called immediately before being removed from OS.exes.
+        /// Called when Result is CompletionResult.Failure.
+        /// </summary>
+        public virtual void OnCompleteFailure() {}
+        /// <summary>
+        /// Called immediately before being removed from OS.exes.
+        /// Called when Result is CompletionResult.Killed.
+        ///
+        /// </summary>
+        public virtual void OnCompleteKilled() {}
+        /// <summary>
+        /// Called immediately before being removed from OS.exes.
+        /// Called when Result is CompletionResult.Success.
+        /// </summary>
+        public virtual void OnCompleteSuccess() {}
+        /// <summary>
+        /// Called immediately before being removed from OS.exes.
+        /// Called after any dedicated completion method.
+        /// </summary>
+        public virtual void OnComplete() {}
+
+        /// <summary>
+        /// Called when not enough ram is available to run the executable
+        /// </summary>
+        public virtual void OnNoAvailableRam() {}
+        /// <summary>
+        /// Called when the computer has an active proxy and the executable could not bypass it
+        /// </summary>
+        public virtual void OnProxyBypassFailure() {}
+        /// <summary>
+        /// Called every Update when Result is CompletitionResult.Running
+        /// </summary>
+        /// <param name="delta">The time since the last OS.Update call</param>
+        public virtual void OnUpdate(float delta) {}
+
+        /// <summary>
+        /// Called when an exception in thrown for this executable
+        /// </summary>
+        /// <param name="exception">The thrown exception</param>
+        /// <returns>True if the exception is caught, false if it propagates</returns>
+        public virtual bool CatchException(Exception exception) { return false; }
+
+        public sealed override void LoadContent() {}
+
+        public sealed override void Completed()
+        {
+            try
+            {
+                switch(Result)
+                {
+                    case CompletionResult.Success:
+                        OnCompleteSuccess();
+                        break;
+                    case CompletionResult.Failure:
+                        OnCompleteFailure();
+                        break;
+                    case CompletionResult.Killed:
+                        OnCompleteKilled();
+                        break;
+                    case CompletionResult.Error:
+                        OnCompleteError();
+                        break;
+                }
+                OnComplete();
+
+                if(Result == CompletionResult.Failure
+                    || Result == CompletionResult.Error
+                    && ErrorReturn != null)
+                    os.write($"{Args[0]}: {(Result == CompletionResult.Failure ? $"failed" : "errored")} with '{ErrorReturn}'");
+            }
+            catch(Exception e)
+            {
+                if(CatchException(e))
+                    throw e;
+            }
+        }
+
+        public sealed override void Killed()
+        {
+            Result = CompletionResult.Killed;
+            needsRemoval = true;
+        }
+
+        public sealed override void Update(float t)
+        {
+            if(Result != CompletionResult.Running) return;
+            Lifetime += t;
+            try
+            {
+                OnUpdate(t);
+            }
+            catch(Exception e)
+            {
+                if(CatchException(e))
+                    throw e;
+            }
+        }
+
+        public override string GetIdentifier()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
