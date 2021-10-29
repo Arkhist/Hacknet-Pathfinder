18dcae0dcc0fd8baa4aba6d268167aeb495f4da2
George L. Albany <Megacake1234@gmail.com>
2021-10-29 04:32:00 -0400

    Move Updater's Loading behavior to LoadAsync Add PATHFINDER_RELEASE_URL const to Updater Add using statements to Updater Trimmed superfluous whitespace

--- PathfinderUpdater/PathfinderUpdaterPlugin.cs
+++ PathfinderUpdater/PathfinderUpdaterPlugin.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Hacknet;
@@ -33,6 +34,8 @@ namespace PathfinderUpdater
         public const string ModGUID = "com.Pathfinder.Updater";
         public const string ModName = "AutoUpdater";
 
+        public const string PATHFINDER_RELEASE_URL = "https://api.github.com/repos/Arkhist/Hacknet-Pathfinder/releases";
+
         new internal static ConfigFile Config;
 
         internal static ConfigEntry<bool> IsEnabled;
@@ -45,7 +48,7 @@ namespace PathfinderUpdater
         public override bool Load()
         {
             Config = base.Config;
-            
+
             IsEnabled = Config.Bind<bool>("AutoUpdater", "EnableAutoUpdates", true, "Enables or disables automatic updates for PathfinderAPI");
             EnablePreReleases = Config.Bind<bool>("AutoUpdater", "UsePreReleases", false, "Whether or not to automatically update to beta versions");
             AcceptedUpdate = Config.Bind<string>("AutoUpdater", "LatestAcceptedUpdate", "", "Used internally to keep track of whether you accepted the update or not");
@@ -66,65 +69,74 @@ namespace PathfinderUpdater
 
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("a", "1"));
-            
-            JArray releases;
-            try
-            {
-                releases = JArray.Parse(client
-                    .GetAsync("https://api.github.com/repos/Arkhist/Hacknet-Pathfinder/releases").Result.Content
-                    .ReadAsStringAsync().Result);
-            }
-            catch (Exception e)
-            {
-                Log.Log(LogLevel.Error, e);
-                client.Dispose();
-                return true;
-            }
-
-            Version tag = null;
-            JToken release = null;
-            foreach (var possibleRelease in releases)
-            {
-                var possibleTag = Version.Parse(possibleRelease.Value<string>("tag_name").Substring(1));
-                if (possibleTag.PreRelease != null && !EnablePreReleases.Value)
-                    continue;
-
-                tag = possibleTag;
-                release = possibleRelease;
-                break;
-            }
-
-            if (HacknetChainloader.Version.Major != tag.Major)
-            {
-                Log.LogWarning($"Latest version of Pathfinder is major {tag.Major}, which is higher than Chainloader version {HacknetChainloader.Version.Major}. Please update manually!");
-            }
-
-            if (Version.Parse(CurrentVersion.Value).Equals(tag))
-                return true;
-            
-            if (tag.ToString() != AcceptedUpdate.Value)
-            {
-                VersionToRequest = tag;
-                return true;
-            }
-            
-            var archive = new ZipArchive(client.GetAsync(release["assets"].First(x => x.Value<string>("name") == "Pathfinder.Release.zip").Value<string>("browser_download_url")).Result.Content.ReadAsStreamAsync().Result);
-            var pfapiPath = Directory.GetFiles(Paths.PluginPath, "PathfinderAPI.dll", SearchOption.AllDirectories)[0];
-
-            File.Delete(pfapiPath);
-            var file = File.OpenWrite(pfapiPath);
-            archive.GetEntry("BepInEx/plugins/PathfinderAPI.dll").Open().CopyTo(file);
-            file.Flush();
-            file.Dispose();
-            
-            archive.Dispose();
-            client.Dispose();
-
-            CurrentVersion.Value = tag.ToString();
+            Task.Run(async () => await LoadAsync());
 
             return true;
+        }
+
+        private async Task LoadAsync()
+        {
+            var pfapiPath = Directory.GetFiles(Paths.PluginPath, "PathfinderAPI.dll", SearchOption.AllDirectories)[0];
+
+            using(var client = new HttpClient())
+            using(var file = File.OpenWrite(pfapiPath))
+            {
+                client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("a", "1"));
+
+                JArray releasesList;
+                try
+                {
+                    var releaseRepoData = await client.GetAsync(PATHFINDER_RELEASE_URL);
+                    var releaseDataContent = await releaseRepoData.Content.ReadAsStringAsync();
+                    releasesList = JArray.Parse(releaseDataContent);
+                }
+                catch (Exception e)
+                {
+                    Log.Log(LogLevel.Error, e);
+                    return;
+                }
+
+                Version tag = null;
+                JToken release = null;
+                foreach (var possibleRelease in releasesList)
+                {
+                    var possibleTag = Version.Parse(possibleRelease.Value<string>("tag_name").Substring(1));
+                    if (possibleTag.PreRelease != null && !EnablePreReleases.Value)
+                        continue;
+
+                    tag = possibleTag;
+                    release = possibleRelease;
+                    break;
+                }
+
+                if (HacknetChainloader.Version.Major != tag.Major)
+                    Log.LogWarning($"Latest version of Pathfinder is major {tag.Major}, which is higher than Chainloader version {HacknetChainloader.Version.Major}. Please update manually!");
+
+                if (Version.Parse(CurrentVersion.Value) == tag)
+                    return;
+
+                if (tag.ToString() != AcceptedUpdate.Value)
+                {
+                    VersionToRequest = tag;
+                    return;
+                }
+
+                var releaseData = await client.GetAsync(
+                    release["assets"]
+                    .First(x => x.Value<string>("name") == "Pathfinder.Release.zip")
+                    .Value<string>("browser_download_url")
+                );
+                var releaseStream = await releaseData.Content.ReadAsStreamAsync();
+
+                using(var archive = new ZipArchive(releaseStream))
+                {
+                    File.Delete(pfapiPath);
+                    archive.GetEntry("BepInEx/plugins/PathfinderAPI.dll").Open().CopyTo(file);
+                    file.Flush();
+                }
+
+                CurrentVersion.Value = tag.ToString();
+            }
         }
 
         [HarmonyPrefix]
@@ -144,7 +156,7 @@ namespace PathfinderUpdater
         private static PFButton AcceptVersion = new PFButton(760, 330, 120, 30, "Yes", new Color(102,255,127));
         private static PFButton DenyVersion = new PFButton(900, 330, 120, 30, "No", new Color(255,92,87));
         private static PFButton SkipVersion = new PFButton(1040, 330, 120, 30, "Skip", new Color(255, 255, 87));
-        
+
         [MethodImpl(MethodImplOptions.NoInlining)]
         internal static void PFAPILoaded()
         {
@@ -164,7 +176,7 @@ namespace PathfinderUpdater
         {
             if (PathfinderUpdaterPlugin.VersionToRequest == null)
                 return;
-            
+
             GuiData.spriteBatch.Draw(Utils.white, new Rectangle(0, 0, GuiData.spriteBatch.GraphicsDevice.Viewport.Width, GuiData.spriteBatch.GraphicsDevice.Viewport.Height), new Color(0, 0, 0, 0.65f));
             GuiData.spriteBatch.Draw(Utils.white, new Rectangle(700, 250, 500, 300), Color.Black);
             TextItem.doLabel(new Vector2(750, 260), $"New Pathfinder Version {PathfinderUpdaterPlugin.VersionToRequest}", Color.White);
