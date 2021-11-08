c79abc6bb31cf5cab35053c090b10dc8bf04bf90
George L. Albany <Megacake1234@gmail.com>
2021-11-08 16:18:54 -0500

    Reworked Updater's UX behaviors and annoyances 	Added Check for update and Update buttons to the top right of the Main Menu 	Moved as much behavior to async tasks as possible 	Added game restart with opt-out prompt 	Makes a single call to Github APIs except in pressing check update 	Checks for update upon starting up 	Notifies the player when the update in complete 	Enabled describing plugin updater info with API attributes 	Deletes temporary files when possible
    Added Meta.UpdaterAttribute
    	Currently only functional for Pathfinder whitelist
    Separated Updater classes into new files
    Added Updater class for handling plugin specific updater behavior
    	Performs all behaviors and checks using async as much as possible
    	Makes one call to each unique API url, caching the results, unless otherwise forced
    	Manages deleting temporary files when possible
    	Can receive custom functions (with active defaults) for:
    		Finding the latest version
    		Getting the update stream
    		Handling the download
    	Can attempt to create an updater for plugin
    	Can read Tuple<string, string, string, bool (optional)> field or property with the name __UpdateData in place of UpdaterAttribute
    		For HacknetChainloader
    Added RestartPopupScreen for restart prompt
    Added NoRestartPrompt option for the prompt, available also in the Updater options menu
    Simplified PFWrapper and renamed it to MainMenuOverride

--- PathfinderUpdater/PathfinderUpdaterPlugin.cs
+++ PathfinderUpdater/PathfinderUpdaterPlugin.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Data;
+using System.Net.Mime;
+using System;
 using System.Configuration;
 using System.IO;
 using System.IO.Compression;
@@ -6,27 +8,29 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Hacknet;
 using BepInEx.Logging;
 using Hacknet;
-using Hacknet.Gui;
 using HarmonyLib;
-using Microsoft.Xna.Framework;
 using Newtonsoft.Json.Linq;
-using Pathfinder.Event;
-using Pathfinder.Event.Menu;
-using Pathfinder.Event.Options;
-using Pathfinder.GUI;
-using Pathfinder.Options;
 using Version = SemanticVersioning.Version;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Pathfinder.GUI;
+using System.Diagnostics;
+using System.Reflection;
+using Pathfinder.Meta;
 
 namespace PathfinderUpdater
 {
     [BepInPlugin(ModGUID, ModName, HacknetChainloader.VERSION)]
+    [Updater(
+        "https://api.github.com/repos/Arkhist/Hacknet-Pathfinder/releases",
+        "Pathfinder.Release.zip",
+        "BepInEx/plugins/PathfinderUpdater.dll"
+    )]
     [HarmonyPatch]
     public class PathfinderUpdaterPlugin : HacknetPlugin
     {
@@ -37,156 +41,121 @@ namespace PathfinderUpdater
 
         internal static ConfigEntry<bool> IsEnabled;
         internal static ConfigEntry<bool> EnablePreReleases;
+        internal static ConfigEntry<bool> NoRestartPrompt;
         internal static ConfigEntry<string> AcceptedUpdate;
         internal static ConfigEntry<string> CurrentVersion;
 
         public static Version VersionToRequest = null;
+        public static bool NeedsUpdate;
+        private static Updater PathfinderUpdater;
+
+        public static readonly List<Updater> Updaters = new List<Updater>();
+        private static readonly List<string> UpdaterWhitelistGuids = new List<string>
+        {
+            "com.Pathfinder.API",
+            "com.Pathfinder.Updater"
+        };
 
         public override bool Load()
         {
             Config = base.Config;
-            
+
             IsEnabled = Config.Bind<bool>("AutoUpdater", "EnableAutoUpdates", true, "Enables or disables automatic updates for PathfinderAPI");
             EnablePreReleases = Config.Bind<bool>("AutoUpdater", "UsePreReleases", false, "Whether or not to automatically update to beta versions");
             AcceptedUpdate = Config.Bind<string>("AutoUpdater", "LatestAcceptedUpdate", "", "Used internally to keep track of whether you accepted the update or not");
             CurrentVersion = Config.Bind<string>("AutoUpdater", "CurrentVersion", HacknetChainloader.VERSION,
                 "Used internally to keep track of version.\nIf you want to skip updating to a version but keep the updater on, set this manually to the latest verison.");
+            NoRestartPrompt = Config.Bind<bool>("AutoUpdater", "NoRestartPrompt", false, "Whether ot not the restart prompt will automatically appear when the update is finished");
 
-            HarmonyInstance.PatchAll(typeof(PathfinderUpdaterPlugin));
+            HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
             if (Type.GetType("Mono.Runtime") != null)
-            {
-                HarmonyInstance.Patch(
-                    AccessTools.Method(
-                        typeof(ConfigurationManager), "OpenExeConfigurationInternal"),
-                    prefix: new HarmonyMethod(AccessTools.Method(typeof(PathfinderUpdaterPlugin), nameof(FixConfig))));
-            }
+                HarmonyInstance.CreateClassProcessor(typeof(MonoRuntimeFixConfig), true).Patch();
 
             if (!IsEnabled.Value)
                 return true;
 
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("a", "1"));
-            
-            JArray releases;
-            try
-            {
-                releases = JArray.Parse(client
-                    .GetAsync("https://api.github.com/repos/Arkhist/Hacknet-Pathfinder/releases").Result.Content
-                    .ReadAsStringAsync().Result);
-            }
-            catch (Exception e)
-            {
-                Log.Log(LogLevel.Error, e);
-                client.Dispose();
-                return true;
-            }
-
-            Version tag = null;
-            JToken release = null;
-            foreach (var possibleRelease in releases)
-            {
-                var possibleTag = Version.Parse(possibleRelease.Value<string>("tag_name").Substring(1));
-                if (possibleTag.PreRelease != null && !EnablePreReleases.Value)
-                    continue;
-
-                tag = possibleTag;
-                release = possibleRelease;
-                break;
-            }
-
-            if (HacknetChainloader.Version.Major != tag.Major)
-            {
-                Log.LogWarning($"Latest version of Pathfinder is major {tag.Major}, which is higher than Chainloader version {HacknetChainloader.Version.Major}. Please update manually!");
-            }
-
-            if (Version.Parse(CurrentVersion.Value).Equals(tag))
-                return true;
-            
-            if (tag.ToString() != AcceptedUpdate.Value)
-            {
-                VersionToRequest = tag;
-                return true;
-            }
-            
-            var archive = new ZipArchive(client.GetAsync(release["assets"].First(x => x.Value<string>("name") == "Pathfinder.Release.zip").Value<string>("browser_download_url")).Result.Content.ReadAsStreamAsync().Result);
-            var pfapiPath = Directory.GetFiles(Paths.PluginPath, "PathfinderAPI.dll", SearchOption.AllDirectories)[0];
-
-            File.Delete(pfapiPath);
-            var file = File.OpenWrite(pfapiPath);
-            archive.GetEntry("BepInEx/plugins/PathfinderAPI.dll").Open().CopyTo(file);
-            file.Flush();
-            file.Dispose();
-            
-            archive.Dispose();
-            client.Dispose();
-
-            CurrentVersion.Value = tag.ToString();
-
             return true;
         }
 
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(Program), nameof(Program.Main))]
-        private static void WaitToPFAPI() => PFWrapper.PFAPILoaded();
-
-        private static void FixConfig(out string exePath)
+        public override void PostLoad()
         {
-            exePath = Paths.ExecutablePath;
+            if(!IsEnabled.Value) return;
+            if(!Updater.TryCreate(typeof(HacknetChainloader), out var chainLoaderUpdater, true))
+                throw new InvalidOperationException("HacknetChainloader missing __UpdaterData");
+            Updaters.Add(chainLoaderUpdater);
+            chainLoaderUpdater.TryDeleteDllTempFile();
+            chainLoaderUpdater.IncludePrerelease = EnablePreReleases.Value;
+            chainLoaderUpdater.CurrentVersion = CurrentVersion.Value;
+
+            foreach(var kvPair in HacknetChainloader.Instance.Plugins)
+            {
+                if(!UpdaterWhitelistGuids.Contains(kvPair.Key)) continue;
+                var type = kvPair.Value.Instance.GetType();
+                if(!Updater.TryCreate(type, out var updater))
+                    continue;
+                Updaters.Add(updater);
+                updater.TryDeleteDllTempFile();
+                updater.IncludePrerelease = EnablePreReleases.Value;
+                updater.CurrentVersion = CurrentVersion.Value;
+                if(kvPair.Key == UpdaterWhitelistGuids[0])
+                    PathfinderUpdater = updater;
+            }
+            PerformUpdateCheck();
+        }
+
+        public static void RestartForUpdate()
+        {
+            AcceptedUpdate.Value = VersionToRequest.ToString();
+            MusicManager.stop();
+            Game1.threadsExiting = true;
+            Game1.getSingleton().Exit();
+            var cmdLine = Environment.CommandLine.Replace(Environment.GetCommandLineArgs()[0] + " ", "");
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = Assembly.GetExecutingAssembly().Location,
+                Arguments = cmdLine
+            });
+            Environment.Exit(0);
+        }
+
+        public static async Task PerformUpdateAsync()
+        {
+            foreach(var updater in Updaters)
+            {
+                await updater.PerformDownload();
+                updater.PerformUpdate();
+                updater.TryDeleteDownloadedTempFile();
+            }
+        }
+
+        public static void PerformUpdateCheck()
+        {
+            Task.Run(async() => await PFWrapper.PerformCheckAndUpdateButtonAsync());
+        }
+
+        public static async Task<bool[]> PerformCheckAsync(bool forceData = false)
+        {
+            var checks = new List<bool>(Updaters.Count);
+            if(forceData)
+                await Updater.ForceResetAllReleaseDataAsync();
+            foreach(var updater in Updaters)
+            {
+                checks.Add(await updater.CheckForUpdate());
+                VersionToRequest = updater.LatestVersion;
+            }
+            return checks.ToArray();
         }
     }
 
-    internal static class PFWrapper
+    public static class MonoRuntimeFixConfig
     {
-        private static OptionCheckbox IsEnabledBox = new OptionCheckbox("Enabled", "Enables the auto updater", PathfinderUpdaterPlugin.IsEnabled.Value);
-        private static OptionCheckbox IncludePrerelease = new OptionCheckbox("IncludePreReleases", "Autoupdate to pre-releases", PathfinderUpdaterPlugin.EnablePreReleases.Value);
-        private static PFButton AcceptVersion = new PFButton(760, 330, 120, 30, "Yes", new Color(102,255,127));
-        private static PFButton DenyVersion = new PFButton(900, 330, 120, 30, "No", new Color(255,92,87));
-        private static PFButton SkipVersion = new PFButton(1040, 330, 120, 30, "Skip", new Color(255, 255, 87));
-        
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        internal static void PFAPILoaded()
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ConfigurationManager), "OpenExeConfigurationInternal")]
+        private static void Prefix(out string exePath)
         {
-            OptionsManager.AddOption("Updater", IsEnabledBox);
-            OptionsManager.AddOption("Updater", IncludePrerelease);
-            EventManager<CustomOptionsSaveEvent>.AddHandler(OptionsSaved);
-            EventManager<DrawMainMenuEvent>.AddHandler(OnDrawMainMenu);
-        }
-
-        internal static void OptionsSaved(CustomOptionsSaveEvent args)
-        {
-            PathfinderUpdaterPlugin.IsEnabled.Value = IsEnabledBox.Value;
-            PathfinderUpdaterPlugin.EnablePreReleases.Value = IncludePrerelease.Value;
-        }
-
-        internal static void OnDrawMainMenu(MainMenuEvent args)
-        {
-            if (PathfinderUpdaterPlugin.VersionToRequest == null)
-                return;
-            
-            GuiData.spriteBatch.Draw(Utils.white, new Rectangle(0, 0, GuiData.spriteBatch.GraphicsDevice.Viewport.Width, GuiData.spriteBatch.GraphicsDevice.Viewport.Height), new Color(0, 0, 0, 0.65f));
-            GuiData.spriteBatch.Draw(Utils.white, new Rectangle(700, 250, 500, 300), Color.Black);
-            TextItem.doLabel(new Vector2(750, 260), $"New Pathfinder Version {PathfinderUpdaterPlugin.VersionToRequest}", Color.White);
-            TextItem.doSmallLabel(new Vector2(750, 300), "Do you want to update? Yes will close the game.", Color.White);
-            if (AcceptVersion.Do())
-            {
-                PathfinderUpdaterPlugin.AcceptedUpdate.Value = PathfinderUpdaterPlugin.VersionToRequest.ToString();
-                MusicManager.stop();
-                Game1.threadsExiting = true;
-                Game1.getSingleton().Exit();
-            }
-            else if (DenyVersion.Do())
-            {
-                PathfinderUpdaterPlugin.VersionToRequest = null;
-                EventManager<DrawMainMenuEvent>.RemoveHandler(OnDrawMainMenu);
-            }
-            else if (SkipVersion.Do())
-            {
-                PathfinderUpdaterPlugin.CurrentVersion.Value = PathfinderUpdaterPlugin.VersionToRequest.ToString();
-                PathfinderUpdaterPlugin.VersionToRequest = null;
-                EventManager<DrawMainMenuEvent>.RemoveHandler(OnDrawMainMenu);
-            }
+            exePath = Paths.ExecutablePath;
         }
     }
 }
