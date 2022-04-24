7bd2c3f2e1c04e3ffde6829d6ea890e1f5647e95
SoundOfScooting <soundofsealing@gmail.com>
2022-04-24 15:25:50 -0500

    Remove null check
    x.Value will need to be changed if #160 is merged.

--- PathfinderAPI/Executable/ExecutableManager.cs
+++ PathfinderAPI/Executable/ExecutableManager.cs
@@ -169,7 +169,7 @@ public static class ExecutableManager
         c.Emit(OpCodes.Ldarg_1);
 
         c.EmitDelegate<Func<Folder, int, OS, bool>>((folder, i, os) => {
-            if(CustomExes.Any(x => x != null && x.Value.ExeData == folder.files[i].data)){
+            if(CustomExes.Any(x => x.Value.ExeData == folder.files[i].data)){
                 os.write(folder.files[i].name.Replace(".exe", ""));
                 return true;
             }
