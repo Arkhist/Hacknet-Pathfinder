14e473897b8d0473c56f296dd07e05e5a6766572
SoundOfScooting <soundofsealing@gmail.com>
2022-10-18 09:40:03 -0500

    Add `xmlName` to `AdministratorAttribute`.

--- PathfinderAPI/Meta/Load/AdministratorAttribute.cs
+++ PathfinderAPI/Meta/Load/AdministratorAttribute.cs
@@ -4,15 +4,24 @@ using Pathfinder.Administrator;
 
 namespace Pathfinder.Meta.Load;
 
-[AttributeUsage(AttributeTargets.Class)]
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
 public class AdministratorAttribute : BaseAttribute
 {
+    public string XmlName { get; }
+
     public AdministratorAttribute()
     {
+    }
+    public AdministratorAttribute(string xmlName)
+    {
+        this.XmlName = xmlName;
     }
 
     protected internal override void CallOn(HacknetPlugin plugin, MemberInfo targettedInfo)
     {
-        AdministratorManager.RegisterAdministrator((Type)targettedInfo);
+        if (XmlName == null)
+            AdministratorManager.RegisterAdministrator((Type)targettedInfo);
+        else
+            AdministratorManager.RegisterAdministrator((Type)targettedInfo, XmlName);
     }
 }
