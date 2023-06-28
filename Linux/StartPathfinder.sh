bd1da8ba78c62bc8580b49364a96fb446f19a429
Fayti1703 <fayti1703@protonmail.com>
2023-06-28 23:30:29 +0200

    Use $() instead of `` in launch script

--- Linux/StartPathfinder.sh
+++ Linux/StartPathfinder.sh
@@ -1,4 +1,4 @@
-cd "`dirname "$0"`"
+cd "$(dirname "$0")"
 
 if [ "$(uname -m)" == "x86_64" ]; then
     LD_PRELOAD="$(pwd)/lib64/libcef.so  $(pwd)/intercept.so /usr/lib/libmono-2.0.so" MONO_DEBUG=explicit-null-checks ./HacknetPathfinder.bin.x86_64 "$@"
