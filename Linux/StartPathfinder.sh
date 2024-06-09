22ffdff321cbee0c7e03b9df164d7841a114db53
Aaron Robinson <aaronrobin1234@gmail.com>
2024-06-08 20:05:04 -0500

    Move all code to a src/ directory

--- Linux/StartPathfinder.sh
+++
@@ -1,7 +0,0 @@
-cd "$(dirname "$0")"
-
-if [ "$(uname -m)" = "x86_64" ]; then
-    LD_PRELOAD="$(pwd)/intercept.so /usr/lib/libmono-2.0.so" MONO_DEBUG=explicit-null-checks ./HacknetPathfinder.bin.x86_64 "$@"
-else
-    LD_PRELOAD="$(pwd)/intercept.so /usr/lib/libmono-2.0.so" MONO_DEBUG=explicit-null-checks ./HacknetPathfinder.bin.x86 "$@"
-fi
