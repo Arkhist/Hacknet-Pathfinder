3b914fd16def4800c2793ff1b4bcd299c38a1c93
Lilith <lsong@princessrtfm.com>
2024-08-01 15:28:23 -0400

    Add patch to avoid null-deref in ThemeChanger
    If you try to use ThemeChanger while you don't have an x-server.sys file, the game attempts to reference a FileEntry object that's null, preventing your theme from being changed. This patch creates a new (empty) x-server.sys file in the event that one doesn't yet exist, both preventing a null reference error in the console and allowing ThemeChanger to work in such cases.

---
+++ PathfinderAPI/BaseGameFixes/AvoidNullDerefOnThemeChange.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Reflection;
+using Hacknet;
+using HarmonyLib;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+
+namespace Pathfinder.BaseGameFixes;
+
+[HarmonyPatch]
+internal static class AvoidNullDerefOnThemeChange {
+
+	[HarmonyILManipulator]
+	[HarmonyPatch(typeof(ThemeChangerExe), nameof(ThemeChangerExe.ApplyTheme))]
+	internal static void ForcePlayerXServerToExist(ILContext context, MethodBase original) {
+		if (original is null)
+			return;
+
+		ILCursor il = new(context);
+		ILLabel fileIsNull = null!;
+		ILLabel afterConditionals = il.DefineLabel();
+		Type str = typeof(string);
+		FieldInfo strEmpty = AccessTools.Field(str, nameof(string.Empty));
+		ConstructorInfo fileCtor = AccessTools.Constructor(typeof(FileEntry), [str, str]);
+		FieldInfo folderFiles = AccessTools.Field(typeof(Folder), nameof(Folder.files));
+		MethodInfo fileListAdd = AccessTools.Method(folderFiles.FieldType, nameof(Folder.files.Add));
+
+		il.GotoNext([ // doesn't matter where we move relative to these, we just need the label at the end of this instruction set
+			i => i.MatchLdloc(2),
+			i => i.MatchLdnull(),
+			i => i.MatchCeq(),
+			i => i.MatchStloc(6),
+			i => i.MatchLdloc(6),
+			i => i.MatchBrtrue(out fileIsNull),
+		]);
+
+		il.GotoLabel(fileIsNull, MoveType.Before); // we need to insert a branch after making the backup, in order to NOT make a duplicate file
+		il.Emit(OpCodes.Br, afterConditionals); // coming in from the part that ran when FileEntry is NOT null, making the backup theme file
+		il.MoveAfterLabels(); // now we need to inject AT the jump target for when the FileEntry IS null
+
+		il.Emit(OpCodes.Ldsfld, strEmpty); // [string.Empty]
+		il.Emit(OpCodes.Ldstr, "x-server.sys"); // [string.Empty, "x-server.sys"]
+		il.Emit(OpCodes.Newobj, fileCtor); // [FileEntry] new FileEntry(string.Empty, "x-server.sys")
+		il.Emit(OpCodes.Stloc_2); // []
+		il.Emit(OpCodes.Ldloc_1); // [Folder]
+		il.Emit(OpCodes.Ldfld, folderFiles); // [Folder.files]
+		il.Emit(OpCodes.Ldloc_2); // [Folder.files, FileEntry]
+		il.Emit(OpCodes.Callvirt, fileListAdd); // [] Folder.files.Add(FileEntry)
+
+		afterConditionals.Target = il.Next;
+	}
+
+}
