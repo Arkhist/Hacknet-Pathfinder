9c24e69a3ea61f0daf444aab5cb52b8e1a8b92e6
Autumn Rivers <natirivers@protonmail.ch>
2024-08-02 17:36:35 -0400

    Add sequencer node dimming fix

---
+++ PathfinderAPI/BaseGameFixes/SequencerNodeDimmingFix.cs
@@ -0,0 +1,17 @@
+﻿using Hacknet;
+
+using HarmonyLib;
+
+namespace Pathfinder.BaseGameFixes;
+
+[HarmonyPatch]
+public class SequencerNodeDimmingFix
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ExtensionSequencerExe), "Killed")]
+    static void FixSequencerNodeDimming(ExtensionSequencerExe __instance)
+    {
+        // This fixes a bug where nodes would continue to be dimmed after the sequencer is killed
+        __instance.os.netMap.DimNonConnectedNodes = false;
+    }
+}
